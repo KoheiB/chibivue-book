f7c0a0349adb0afd116b0cdc4b337e39c07dd669
Kohei Terashita <kou0704terasita@gmail.com>
2023-07-17 20:15:18 +0900

    simple_h_function

--- packages/runtime-dom/index.ts
+++ packages/runtime-dom/index.ts
@@ -1,13 +1,12 @@
-// ~/packages/runtime-dom/index.ts
-
 import {
   CreateAppFunction,
   createAppAPI,
   createRenderer,
 } from "../runtime-core";
 import { nodeOps } from "./nodeOps";
+import { patchProp } from "./patchProp";
 
-const { render } = createRenderer(nodeOps);
+const { render } = createRenderer({ ...nodeOps, patchProp });
 const _createApp = createAppAPI(render);
 
 export const createApp = ((...args) => {
